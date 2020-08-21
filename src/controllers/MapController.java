3601dd708acb479cf3acccb20d8c0bb0b3ecd1a8
Kyaw Myo Tun <kyawmyotun472001@gmail.com>
2020-08-21 20:01:31 +0630

    add Map to MapController

--- src/controllers/MapController.java
+++ src/controllers/MapController.java
@@ -1,26 +1,47 @@
 package controllers;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-public class MapController {
 
+public class MapController {
+    WebEngine webEngine =null;
     @FXML
     private BorderPane borderPane;
     @FXML
-    private JFXButton id;
+    private JFXButton myanmar;
+
     @FXML
-    void initialize(){
+    private JFXButton london;
 
-            System.out.println(borderPane);
-
-       //     borderPane.setCenter(new Button("Hello World"));
+    @FXML
+    void onLondon(ActionEvent event) {
+       webEngine.executeScript("alert('Hello')");
     }
 
+    @FXML
+    void onMyanamar(ActionEvent event) {
+        System.out.println("From Myanmar");
+
+    }
+    @FXML
+    void initialize() {
+        Platform.runLater(() -> {
+            System.out.println(borderPane);
+            WebView webView = new WebView();
+            webEngine =webView.getEngine();
+            webEngine.load(getClass().getResource("../views/map.html").toString());
+            borderPane.setCenter(new HBox(webView));
+        });
+
+    }
 
 
 }
