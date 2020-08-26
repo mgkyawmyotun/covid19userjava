cebee00c950fc4001fcaf8f16c51bfde26fbd4c6
Kyaw Myo Tun <kyawmyotun472001@gmail.com>
2020-08-26 11:56:07 +0630

    add Create User

--- src/models/UserModel.java
+++ src/models/UserModel.java
@@ -7,30 +7,38 @@ import java.io.IOException;
 import java.sql.*;
 
 public class UserModel {
-
+    public static final String URI = "http://localhost:1337/user/";
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     OkHttpClient okHttpClient;
+
     public UserModel() {
-        okHttpClient =new OkHttpClient();
+        okHttpClient = new OkHttpClient();
     }
-    public JSONObject getUser(){
-        Request request =new Request.Builder().url("https://disease.sh/v3/covid-19/all").build();
-        String response =null;
+
+    public JSONObject getUser() {
+        Request request = new Request.Builder().url("https://disease.sh/v3/covid-19/all").build();
+        String response = null;
         try {
-             response =okHttpClient.newCall(request).execute().body().string();
+            response = okHttpClient.newCall(request).execute().body().string();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(response);
 
-        return  new JSONObject(response);
+        return new JSONObject(response);
 
     }
-    public  JSONObject createUser(){
 
-        return  null;
+    public JSONObject createUser(String json) {
+        RequestBody requestBody = RequestBody.create(JSON, json);
+        Request request = new Request.Builder().url(URI + "login").post(requestBody).build();
+        String response = null;
+        try {
+            response = okHttpClient.newCall(request).execute().body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new JSONObject(response);
     }
-
 
 
 }
