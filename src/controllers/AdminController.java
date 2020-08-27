26e6b9856206592bb393d109e295ae53147bee25
youser <you@example.com>
2020-08-27 16:28:49 +0630

    complete admin dashboard

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -1,4 +1,79 @@
-package controllers;
+package views;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class AdminController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private JFXButton state;
+
+    @FXML
+    private JFXButton town;
+
+    @FXML
+    private ImageView townShip;
+
+    @FXML
+    private JFXButton hospital;
+
+    @FXML
+    private JFXButton patient;
+
+    @FXML
+    private JFXButton admin;
+
+    @FXML
+    private JFXButton logout;
+
+    @FXML
+    void onAdmin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onHospital(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLogout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onPatient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onState(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTown(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onTownShip(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
