dd1b3dbd3831fd3e4c49a955ea6e85c54cf6e5c6
Kyaw Myo Tun <kyawmyotun472001@gmail.com>
2020-09-05 11:42:33 +0630

    add Some Chart

--- src/controllers/GlobalChartController.java
+++ src/controllers/GlobalChartController.java
@@ -3,19 +3,22 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXSpinner;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
-import utils.HttpService;
 
 public class GlobalChartController {
+    @FXML
+    private JFXButton caseByContienets;
 
     @FXML
     private JFXSpinner spinner;
@@ -26,15 +29,55 @@ public class GlobalChartController {
 
     @FXML
     private URL location;
+    @FXML
+    private JFXButton caseByCountries;
 
     @FXML
     private BorderPane borderPane;
 
     @FXML
+    private JFXButton dcr10;
+
+    @FXML
     void initialize() {
-        loadMap();
+        loadCaseByContientsChart();
     }
-    private void loadMap() {
+    private void loadCaseByContientsChart() {
+        spinner.setVisible(true);
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
+        webView.setCache(true);
+        webView.setContextMenuEnabled(true);
+        webView.setStyle(null);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(() -> {
+                    webEngine.load(getClass().getResource("/views/GlobalView/globalChart.html").toString());
+
+                    borderPane.setCenter(webView);
+
+
+
+                }  );
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+        task.setOnSucceeded((a) ->{
+
+        });
+
+
+    }
+    @FXML
+    void onCaseByContienets(ActionEvent event) {
+        loadCaseByContientsChart();
+    }
+
+    private void loadCaseByCountries() {
         spinner.setVisible(true);
         webView = new WebView();
         webEngine = webView.getEngine();
@@ -44,43 +87,128 @@ public class GlobalChartController {
         Task<Void> task = new Task<Void>() {
             @Override
             protected Void call() throws Exception {
-//                data = HttpService.getCaseByCountries();
                 Platform.runLater(() -> {
-                    webEngine.load(getClass().getResource("/views/GlobalView/globalChart.html").toString());
+                    webEngine.load(getClass().getResource("/views/GlobalView/globalChartGeo.html").toString());
                     borderPane.setCenter(webView);
-                    webEngine.setOnAlert(e ->{
-                            System.out.println(e.getData());
-                    });
-
 
                 }  );
 
-
-//
-//                    webEngine.getLoadWorker().stateProperty().addListener(
-//                            new ChangeListener() {
-//                                @Override
-//                                public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-//
-//
-//                                    webEngine.executeScript("test(" + data + ")");
-//
-//
-//                                    if (newValue != Worker.State.SUCCEEDED) {
-//                                        return;
-//                                    }
-//
-//                                }
-//                            }
-//                    );
                 return null;
             }
         };
         new Thread(task).start();
-        task.setOnSucceeded((a) ->{
+    }
+    @FXML
+    void onCaseByContries(ActionEvent event) {
+        loadCaseByCountries();
+    }
 
-        });
 
+    private void loaddcr10() {
+        spinner.setVisible(true);
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
+        webView.setCache(true);
+        webView.setContextMenuEnabled(true);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(() -> {
+                    webEngine.load(getClass().getResource("/views/GlobalView/globalChartTime.html").toString());
+                    borderPane.setCenter(webView);
+
+                }  );
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+
+    @FXML
+    void onDcr10(ActionEvent event) {
+        loaddcr10();
+    }
+    private void loadGCases() {
+        spinner.setVisible(true);
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
+        webView.setCache(true);
+        webView.setContextMenuEnabled(true);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(() -> {
+                    webEngine.load(getClass().getResource("/views/GlobalView/globalChartTimeLine.html").toString());
+                    borderPane.setCenter(webView);
+
+                }  );
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+    @FXML
+    void onGCases(ActionEvent event) {
+        loadGCases();
+    }
+    private void loadGDeaths() {
+        spinner.setVisible(true);
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
+        webView.setCache(true);
+        webView.setContextMenuEnabled(true);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(() -> {
+                    webEngine.load(getClass().getResource("/views/GlobalView/globalChartDeathTime.html").toString());
+                    borderPane.setCenter(webView);
+
+                }  );
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+    @FXML
+    void onGDeaths(ActionEvent event) {
+        loadGDeaths();
+    }
+    private void loadGRecover() {
+        spinner.setVisible(true);
+        webView = new WebView();
+        webEngine = webView.getEngine();
+        webEngine.setJavaScriptEnabled(true);
+        webView.setCache(true);
+        webView.setContextMenuEnabled(true);
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater(() -> {
+                    webEngine.load(getClass().getResource("/views/GlobalView/globalChartRecoverLine.html").toString());
+                    borderPane.setCenter(webView);
+
+                }  );
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+    @FXML
+    void onGRecover(ActionEvent event) {
+        loadGRecover();
+    }
+
+
+    @FXML
+    void onAge(ActionEvent event) {
 
     }
 }
