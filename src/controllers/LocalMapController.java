83086ec8c83c8fa502f5ac4818c346f8a954c33b
Kyaw Myo Tun <kyawmyotun472001@gmail.com>
2020-09-06 16:03:09 +0630

    add states

--- src/controllers/LocalMapController.java
+++ src/controllers/LocalMapController.java
@@ -3,21 +3,34 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.jfoenix.controls.*;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import models.LocalModel;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import utils.HttpService;
 
 public class LocalMapController {
 
@@ -48,13 +61,16 @@ public class LocalMapController {
     @FXML
     private TextField search;
 
-    WebEngine webEngine = null;
-    WebView webView = null;
+    private WebEngine webEngine = null;
+    private WebView webView = null;
+    private JFXListView<Label> list;
+    private FilteredList<Label> filteredList;
 
     @FXML
     void initialize() {
         loadData();
         loadMap();
+        loadList();
     }
 
     private void loadData() {
@@ -113,6 +129,51 @@ public class LocalMapController {
 
         });
 
+
+    }
+
+    private void loadList() {
+
+
+        Task<Void> task1 = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                ObservableList<Label> observableList = FXCollections.observableArrayList();
+
+
+                String states[] = {"Yangon", "Sagaing", "Ayeyarwady","Tanintharyi", "Kayah", "Kayin", "Mon", "Shan", "Chin", "Mandalay", "Kachin", "Rakhine", "Nay Pyi Taw", "Bago", "Magway"};
+                for (int i = 0; i < states.length; i++) {
+
+                    Label label = new Label(states[i]);
+
+                    Image image = new Image("/views/Images/state/" + "chin" + ".png");
+                    ImageView imageView = new ImageView(image);
+                    imageView.setFitWidth(50);
+                    imageView.setFitHeight(50);
+                    label.setGraphic(imageView);
+                    observableList.add(label);
+                }
+                Platform.runLater(() -> {
+
+                    filteredList = new FilteredList<Label>(observableList);
+                    list = new JFXListView<>();
+                    list.setItems(filteredList);
+
+                    list.getStylesheets().add("/views/css/listview.css");
+                    list.setCursor(Cursor.HAND);
+                    list.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> labelClicked(e));
+                    vbox.getChildren().remove(vbox.getChildren().size() - 1);
+                    vbox.getChildren().add(list);
+                });
+                return null;
+            }
+        };
+
+        new Thread(task1).start();
+
+    }
+
+    private void labelClicked(MouseEvent e) {
 
     }
 }
